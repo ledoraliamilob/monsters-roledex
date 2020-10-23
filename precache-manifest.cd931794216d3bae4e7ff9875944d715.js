aea590b99218b50e0e8a69ac8155b1426179f164
ledoraliamilob <rodel.bolima@ossinc.co>
2020-10-23 21:03:10 +0800

    Updates

---
+++ precache-manifest.cd931794216d3bae4e7ff9875944d715.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a54c78dfa52a3dfaac262b5b69eb474",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "1fa98bd0796efcffc4fa",
+    "url": "/monsters-roledex/static/css/main.797081c3.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-roledex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-roledex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa98bd0796efcffc4fa",
+    "url": "/monsters-roledex/static/js/main.696deacb.chunk.js"
+  },
+  {
+    "revision": "f978c494dd52a7663e48",
+    "url": "/monsters-roledex/static/js/runtime-main.c1b4038e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-roledex/static/media/logo.5d5d9eef.svg"
+  }
+]);
